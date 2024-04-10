dfcb845ea5f8edfe64f57368460383dd2e8cc794
Maga <maga@LAPTOP-1I6JJP9Q>
2024-04-10 18:02:19 +0300

    realized get amplitude function

--- main.py
+++ main.py
@@ -32,20 +32,6 @@ def read_table(file):
     df = pd.DataFrame(values.values, columns=headers_list)
     return df
 
-def calc_voo_table(mean_parameters):
-    mean_values = mean_parameters.values[0]
-    columns = ['Voo(м/с)', 'Cq', 'Cd', 'Kp', 'T(s)', 'Std', 'Stdо']
-    values = [[
-        '=P2',
-        '=P2',
-        '=P2',
-        '=P2',
-        '=P2',
-        '=P2',
-        '=P2'
-        ]]
-    df = pd.DataFrame(values, columns=columns)
-    return df
 
 def get_float_table(df):
     values = np.array(df.values)
@@ -58,7 +44,7 @@ def get_float_table(df):
 
 def interesting_table(table):
     columns = table.columns
-    num_of_columns = [2, 3, 7, 11]
+    num_of_columns = [0, 2, 3, 7, 11]
     
     interesting_columns = [columns[i] for i in num_of_columns]
     interesting_table = table[interesting_columns].copy()
@@ -67,21 +53,52 @@ def interesting_table(table):
 
 def get_period(mean_parameters):
     float_mean_parameters = get_float_table(mean_parameters)
+    period = 1 / float_mean_parameters['Hk(гц)'][0]
+    return period
+
+def add_period(interesting_table, period):
+    interesting_table['period'] = interesting_table['t'] / period
+    interesting_table['period'] = interesting_table['period'].apply(int)
+    return interesting_table
+
+def amplitudes_table(interesting_table):
+    amplitudes_table = pd.DataFrame()
+    pk_min = []
+    pk_max = []
+    p0_min = []
+    p0_max = []
+    d = {'Pэкран': {'max' : [], 'min' : []}, '(Pk-Pa)': {'max' : [], 'min' : []}}
+    for period in interesting_table['period'].unique():
+        for p_name in d.keys():
+            d[p_name]['min'].append(interesting_table[interesting_table['period'] == period][p_name].min())
+            d[p_name]['max'].append(interesting_table[interesting_table['period'] == period][p_name].max())
+            
+    
+    amplitudes_table['pэ_max'] = d['Pэкран']['max']
+    amplitudes_table['pэ_min'] = d['Pэкран']['min']
+    
+    amplitudes_table['pk_max'] = d['(Pk-Pa)']['max']
+    amplitudes_table['pk_min'] = d['(Pk-Pa)']['min']
+
+    return amplitudes_table
 
 mean_parameters = read_mean_parameters(sys.argv[1])
 table = read_table(sys.argv[1])
-voo_table = calc_voo_table(mean_parameters)
 
 writer = pd.ExcelWriter('file.xlsx', engine='xlsxwriter')
 
 table.to_excel(writer, 'Sheet1')
 mean_parameters.to_excel(writer, 'Sheet1', startcol=15, index=False)
-voo_table.to_excel(writer, 'Sheet1', startcol=15, startrow=5, index=False)
-print(interesting_table(get_float_table(table)).head())
+
+interesting_table = interesting_table(get_float_table(table))
+period = get_period(mean_parameters)
+interesting_table = add_period(interesting_table, period)
+print(amplitudes_table(interesting_table).head())
+
 writer.close()
 
-print(mean_parameters.head())
-print(get_float_table(table).head())
+#print(mean_parameters.head())
+#print(get_float_table(table).head())
 
 
 #print(mean_parameters.apply(pd.to_numeric).head())
